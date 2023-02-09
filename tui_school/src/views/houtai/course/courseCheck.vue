7bec1ef729880a26288de0b357c84523398c3296
wss01piin <3027982926@qq.com>
2023-02-09 17:54:37 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	group_three/sys-comm/src/main/java/com/aaa/entity/GGoods.java
    #	group_three/sys-system/src/main/java/com/aaa/group_three/controller/BBusinessInfoController.java
    #	group_three/sys-system/src/main/java/com/aaa/group_three/controller/GGoodsController.java
    #	group_three/sys-system/src/main/java/com/aaa/group_three/dao/GGoodsMapper.java
    #	group_three/sys-system/src/main/java/com/aaa/group_three/service/IGGoodsService.java
    #	group_three/sys-system/src/main/java/com/aaa/group_three/service/impl/GGoodsServiceImpl.java
    #	group_three/sys-system/src/main/resources/mapper/GGoodsMapper.xml

--- tui_school/src/views/houtai/course/courseCheck.vue
+++ tui_school/src/views/houtai/course/courseCheck.vue
@@ -46,53 +46,6 @@
                 </el-form-item>
             </el-form>
         </div>
-<!--        &lt;!&ndash;修改的弹出框&ndash;&gt;-->
-<!--        <el-dialog-->
-<!--                title="修改"-->
-<!--                :visible.sync="visible"-->
-<!--                @close="closeCllback"-->
-<!--                width="70%">-->
-<!--            <el-form ref="formDataRef" :model="formData" label-width="100px">-->
-<!--                <el-form-item label="课程名称：">-->
-<!--                    <el-input v-model="formData.title"></el-input>-->
-<!--                </el-form-item>-->
-<!--                <el-form-item label="是否免费：">-->
-<!--                    <el-radio-group v-model="formData.price" v-if="formData.price!=0">-->
-<!--                        <el-radio :label="formData.price">收费</el-radio>-->
-<!--                        <el-radio :label="0.00">免费</el-radio>-->
-<!--                    </el-radio-group>-->
-<!--                    <el-radio-group v-model="formData.price" v-if="formData.price==0">-->
-<!--                        <el-radio :label="1">收费</el-radio>-->
-<!--                        <el-radio :label="0.00">免费</el-radio>-->
-<!--                    </el-radio-group>-->
-<!--                </el-form-item>-->
-
-<!--                <el-row v-if="formData.price != 0">-->
-<!--                    <el-col :span="12">-->
-<!--                        <el-form-item label="原价：">-->
-<!--                            <el-input type="text" style="width: 120px" placeholder="请输入价格" v-model="formData.price"></el-input> 元-->
-<!--                        </el-form-item>-->
-<!--                    </el-col>-->
-<!--                </el-row>-->
-<!--                <el-form-item label="课程简介:">-->
-<!--                    &lt;!&ndash;<el-input-->
-<!--                            type="textarea"-->
-<!--                            autosize-->
-<!--                            placeholder="请输入内容"-->
-<!--                            v-model="formData.remark">-->
-<!--                    </el-input>&ndash;&gt;-->
-<!--                    <quill-editor class="editor"-->
-<!--                                  v-model="formData.description"-->
-<!--                                  ref="myQuillEditor"-->
-<!--                                  :options="editorOption">-->
-<!--                    </quill-editor>-->
-<!--                </el-form-item>-->
-<!--                <el-row style="margin-top:17px; ">-->
-<!--                    <el-button style="float:right;margin-left:6px;" size="mini" type="danger" plain @click="visible = false">取 消</el-button>-->
-<!--                    <el-button style="float:right" size="mini" type="primary" @click="submitForm">确定</el-button>-->
-<!--                </el-row>-->
-<!--            </el-form>-->
-<!--        </el-dialog>-->
         <!--审核的弹出框-->
         <el-dialog
                 title="课程审核"
@@ -137,13 +90,13 @@
                     </span>
                 </template>
             </el-table-column>
-            <el-table-column prop="brand.brandname" label="商品品牌" width="80">
+            <el-table-column prop="brandname" label="商品品牌" width="80">
             </el-table-column>
-            <el-table-column prop="sort.sortname" label="商品类别" width="80">
+            <el-table-column prop="sortname" label="商品类别" width="80">
             </el-table-column>
-            <el-table-column prop="deposit.depositname" label="储藏方式" width="80">
+            <el-table-column prop="depositname" label="储藏方式" width="80">
             </el-table-column>
-            <el-table-column prop="unit.unitname" label="商品单位" width="80">
+            <el-table-column prop="unitname" label="商品单位" width="80">
             </el-table-column>
             <el-table-column prop="addtime" label="上架时间" width="180">
             </el-table-column>
@@ -165,21 +118,8 @@
             </el-table-column>
             <el-table-column label="操作" header-align="center" fixed="right" width="300px" align="center" prop="status">
                 <template slot-scope="scope">
-                    <!--<el-button  type="success" @click="handleEdit(scope.row)" size="mini">修改</el-button>-->
                     <el-button  type="primary" @click="handleCheckCourse(scope.row.id)" size="mini" v-if="scope.row.state===2">去审核</el-button>
                     <el-button  type="info" size="mini" v-if="scope.row.state!==2">已审核</el-button>
-
-                    <!--<el-switch
-                            style="margin-left: 20px"
-                            v-model="scope.row.status"
-                            @change="handleChangeIsPutaway(scope.row.id,scope.row.status)"
-                            :active-value="1"
-                            :inactive-value="0"
-                            active-color="#ff4949"
-                            inactive-color="#13ce66"
-                            active-text="下架"
-                            inactive-text="上架">
-                    </el-switch>-->
                 </template>
             </el-table-column>
         </el-table>
@@ -189,7 +129,7 @@
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
                 :page-size="page.pageSize"
-                :page-sizes="[2,5, 8, 10, 15, 20, 30]"
+                :page-sizes="[2,5, 8, 10, 20, 30]"
                 layout="total, sizes, prev, pager, next, jumper"
                 :total="page.totalCount">
         </el-pagination>
@@ -233,14 +173,14 @@
         },
         methods: {
             goodsSort(){
-               this.$http.get("commodity/sort/findAllSort").then(res=>{
+               this.$http.get("syssystem/g-sort/sort").then(res=>{
                    if (res.data.code===2000){
                        this.sorts = res.data.data;
                    }
                })
             },
             initShops(){
-              this.$http.get("/business/selectshop").then(res=>{
+              this.$http.get("/syssystem/b-business-info/getsname").then(res=>{
                   if (res.data.code===2000){
                       console.log(res.data.data)
                       this.shops = res.data.data;
@@ -250,7 +190,7 @@
             // 商品分类分页列表接口
             searchCourse(){
                 var that =this
-                this.$http.post(`/commodity/goods/findAllGoods/${this.page.pageCurrent}/${this.page.pageSize}`,this.map).then(function (resp) {
+                this.$http.post(`/syssystem/g-goods/findAllGoods/${this.page.pageCurrent}/${this.page.pageSize}`,this.map).then(function (resp) {
                     if (resp.data.code===2000){
                         that.list = resp.data.data.records
                         that.page.totalCount = resp.data.data.total
@@ -258,26 +198,10 @@
                     }
                 })
             },
-            //提交修改的表单
-            submitForm(){
-                var that = this
-                this.$http.post(`/core/course/updateCourse`,this.formData).then(function (resp) {
-                    if (resp.data.code==200){
-                        that.$message.success(resp.data.msg);
-                        that.visible = false
-                    }else {
-                        that.$message.error(resp.data.msg);
-                        that.visible = false
-                    }
-                })
-            },
             //提交审核的表单
             submitCheckForm(){
-                /*if (this.checkformData.checkContent === ""){
-                    this.checkformData.checkContent  = "系统默认通过";
-                }*/
                 var that = this
-                this.$http.post(`/commodity/goods/updateState`,this.checkformData).then(function (resp) {
+                this.$http.post(`/syssystem/g-goods/shenhe`,this.checkformData).then(function (resp) {
                     if (resp.data.code===2000){
                         that.$message.success(resp.data.msg);
                         that.checkvisible = false
@@ -329,36 +253,6 @@
                 this.$refs.checkformDataRef.resetFields();
                 this.reload()
             },
-            // 修改上下架状态
-            handleChangeIsPutaway(id, status) {
-                const title = {"1": '下架', "0": '上架'}
-                this.$confirm(`确定要${title[status]}吗?`, {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                    this.changeIsPutaway(id, status)
-                }).catch(() => {
-                    this.reload()
-                })
-            }
-            ,
-            // 请求更新上下架方法
-            changeIsPutaway(id, status) {
-                var that =this
-                this.$http.post(`/commodity/goods/updateStatus`, {"id":id,"status":status}).then(function (resp) {
-                    if (resp.data.code===2000){
-                        that.$message.success(resp.data.msg);
-                    }
-                    that.reload()
-                })
-            },
-            // 修改弹窗
-            // handleEdit(row) {
-            //     this.formData = row
-            //     this.visible = true
-            // },
-            //审核课程
             handleCheckCourse(id,state){
                 this.checkformData.id = id
                 this.checkvisible = true
